78658afea326e18fc88274a1c7f1ca57bf4f860b
Fernando Martinelli <fernando.martinelli@gmail.com>
2012-05-06 22:29:24 +0200

    aaaaFernando 22:29

--- findSub.php
+++ findSub.php
@@ -1,12 +1,18 @@
 <?php 
+
+// testing findsub
+
 include_once('config/mysql-config.php');
-$t=$_GET[topic];
-$query="select * from $TABLE_CATEGORIES where parent_id='$t'";
+$topic=$_GET[topic];
+$query="select * from $TABLE_CATEGORIES where parent_id='$topic'";
 $r=mysql_query($query);
 ?>
 <option value="0">Sub Area</option>
-
 <?php while($row=mysql_fetch_array($r)) {
-print( '<option value='.$row['cat_id'].'>'.$row['cat_title'].'</option>');
+echo '<option value='.$row['cat_id'].'>'.$row['cat_title'].'</option>';
 } 
+
+// Now fernando changed line 15 =) again aaa
+
+// now line 17 fernando
 ?>
