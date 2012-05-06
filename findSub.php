e564b606444d507cc2816d5c84d3123630e1c98e
Manoj Kumar <rvmanojkumar@gmail.com>
2012-05-07 01:41:34 +0530

    changed echo to print

--- findSub.php
+++ findSub.php
@@ -7,9 +7,7 @@ $r=mysql_query($query);
 <option value="0">Sub Area</option>
 
 <?php while($row=mysql_fetch_array($r)) {
-echo '<option value='.$row['cat_id'].'>'.$row['cat_title'].'</option>';
+print( '<option value='.$row['cat_id'].'>'.$row['cat_title'].'</option>');
 
 } 
-
-
 ?>
